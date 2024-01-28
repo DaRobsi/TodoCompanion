d28af40fafd3fc1f9e321d17863c282edf9b4768
DaRobsi <alfarobin388@gmail.com>
2024-01-28 01:17:29 +0100

    added endpoints and api service

--- backend/src/endpoints.rs
+++ backend/src/endpoints.rs
@@ -1,1 +1,22 @@
+use poem_openapi::{payload::*, OpenApi};
 
+use crate::graph_communicator;
+
+pub struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/healthcheck", method = "get")]
+    async fn healthcheck(&self) -> PlainText<&'static str> {
+        PlainText("This works!")
+    }
+
+    #[oai(path = "/graph/me", method = "get")]
+    async fn getting_myself(&self) -> Json<serde_json::value::Value> {
+        Json(
+            graph_communicator::get_self()
+                .await
+                .expect("Something went wrong"),
+        )
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,18 @@
 mod endpoints;
 mod graph_communicator;
 
-//use poem::{listener::TcpListener, Route, Server};
-//use poem_openapi::OpenApiService;
+use poem::{listener::TcpListener, Route, Server};
+use poem_openapi::OpenApiService;
 
 #[tokio::main]
-async fn main(){
-    let result = graph_communicator::get_self().await.unwrap();
-    println!("{}", result);
+async fn main() {
+    let api_service =
+        OpenApiService::new(endpoints::Api, "Hello World", "1.0").server("http://localhost:3000");
+    let ui = api_service.swagger_ui();
+    let app = Route::new().nest("/api", api_service).nest("/docs", ui);
+
+    // prb better to use envvar for port in TodoCompanion
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await;
 }
