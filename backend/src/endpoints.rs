06a3cab16be5807aca0e53fbb5af89dd1a7c667a
DaRobsi <alfarobin388@gmail.com>
2024-01-28 01:50:19 +0100

    added basic logging, made endpoints work through swagger ui

--- backend/src/endpoints.rs
+++ backend/src/endpoints.rs
@@ -1,4 +1,5 @@
 use poem_openapi::{payload::*, OpenApi};
+use log::info;
 
 use crate::graph_communicator;
 
@@ -8,11 +9,13 @@ pub struct Api;
 impl Api {
     #[oai(path = "/healthcheck", method = "get")]
     async fn healthcheck(&self) -> PlainText<&'static str> {
+        info!("Healthcheck");
         PlainText("This works!")
     }
 
     #[oai(path = "/graph/me", method = "get")]
     async fn getting_myself(&self) -> Json<serde_json::value::Value> {
+        info!("Fetching own account...");
         Json(
             graph_communicator::get_self()
                 .await
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,21 @@
 mod endpoints;
 mod graph_communicator;
 
-use poem::{listener::TcpListener, Route, Server};
+use poem::{listener::TcpListener, EndpointExt, Route, Server};
 use poem_openapi::OpenApiService;
+use log::info;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let api_service =
-        OpenApiService::new(endpoints::Api, "Hello World", "1.0").server("http://localhost:3000");
+        OpenApiService::new(endpoints::Api, "Hello World", "1.0").server("http://localhost:3000/api");
     let ui = api_service.swagger_ui();
     let app = Route::new().nest("/api", api_service).nest("/docs", ui);
 
     // prb better to use envvar for port in TodoCompanion
-    Server::new(TcpListener::bind("127.0.0.1:3000"))
+    info!("Starting API service...");
+    let _ = Server::new(TcpListener::bind("127.0.0.1:3000"))
         .run(app)
         .await;
 }
